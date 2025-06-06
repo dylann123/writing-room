b1c9e4d71899c77d707969e09c4f8ed66e613f67
dylann123 <64336537+dylann123@users.noreply.github.com>
2025-06-06 16:12:01 -0400

    im cooking

--- Characters/yellow_man/yellow_man.gd
+++ Characters/yellow_man/yellow_man.gd
@@ -1,13 +1,87 @@
 extends Node2D
 
-var lightpunchres: Resource = null
-var lightpunchinstance: Node2D = null
-var lightkickres: Resource = null
-var lightkickinstance = null
-var heavypunchres: Resource = null
-var heavypunchinstance = null
-var heavykickres: Resource = null
-var heavykickinstance = null
+var actionmap = {
+	"LightPunch": {
+		"ground": {
+			"instance": load("res://Characters/yellow_man/Attacks/LightPunch_Ground.tscn"),
+			"execute": (func():
+			default(actionmap["LightPunch"]["ground"]["instance"])
+			)
+		},
+		"air": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		},
+		"crouched": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		}
+	},
+	"HeavyPunch": {
+		"ground": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		},
+		"air": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		},
+		"crouched": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		}
+	},
+	"LightKick": {
+		"ground": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		},
+		"air": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		},
+		"crouched": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		}
+	},
+	"HeavyKick": {
+		"ground": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		},
+		"air": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		},
+		"crouched": {
+			"res": "",
+			"execute": (func():
+			pass
+			)
+		}
+	}
+}
 
 var Player = null
 
@@ -17,18 +91,20 @@ func _ready():
 	Player = get_parent()
 	var file_name = get_meta("Character")
 	
-	lightpunchres = load("res://Characters/yellow_man/Attacks/LightPunch.tscn")
+	
+func parse_special(type):
+	pass
 
 func LightPunch():
-	if not Player.crouching:
-		lightpunchinstance = lightpunchres.instantiate()
-		var offset = lightpunchinstance.get_node("RectHitbox").position.x * 2
-		add_child(lightpunchinstance)
-		lightpunchinstance.position = $CharacterBody2D.position
-		if Player.direction == -1:
-			lightpunchinstance.position.x *= -1
-			lightpunchinstance.position.x += $CharacterBody2D.position.x * 2 - offset
-		action_frames = lightpunchinstance.get_meta("ActionFrames")
+	if parse_special("LP"):
+		pass
+	elif Player.crouching:
+		actionmap["LightPunch"]["crouched"]["execute"].call()
+	elif not Player.Body.is_on_floor():
+		actionmap["LightPunch"]["air"]["execute"].call()
+	else:
+		actionmap["LightPunch"]["ground"]["execute"].call()
+	
 
 func LightKick():
 	if not Player.crouching:
@@ -42,8 +118,18 @@ func HeavyKick():
 	if not Player.crouching:
 		pass
 
+func default(res):
+	var instance = res.instantiate()
+	var offset = instance.get_node("RectHitbox").position.x * 2
+	add_child(instance)
+	instance.position = $CharacterBody2D.position
+	if Player.direction == -1:
+		instance.position.x *= -1
+		instance.position.x += $CharacterBody2D.position.x * 2 - offset
+	action_frames = instance.get_meta("ActionFrames")
+
 func _physics_process(delta: float) -> void:
-	action_frames -= 1
+	action_frames = clamp(action_frames - 1, -1, 9999)
 	if action_frames == 0:
 		get_parent().can_act = true
 	elif action_frames > 0:
